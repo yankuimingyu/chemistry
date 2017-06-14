d9a3d1aa381c898607a6090cf1f56046072726b6
yankuimingyu <chinayankui@163.com>
2017-06-14 10:06:46 +0800

    EMwin_Yankui -IO-2016-7-25
    EMwin_Yankui -IO-2016-7-25 my remend

--- OBJ/chinese_charcter.d
+++
@@ -1,8 +0,0 @@
-..\obj\chinese_charcter.o: ..\SMDEMO\chiese\chinese_charcter.c
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\GUI.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\GUI_ConfDefaults.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\Config\GUIConf.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\GUI_Type.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\LCD.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\Global.h
-..\obj\chinese_charcter.o: ..\EMWIN\STemWin\inc\GUI_Version.h
